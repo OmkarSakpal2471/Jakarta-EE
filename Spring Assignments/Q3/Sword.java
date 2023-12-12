61362f071b9c9d6f3b7b4c07dde9e21b5f2b84dc
OmkarSakpal2471 <90462472+OmkarSakpal2471@users.noreply.github.com>
2023-12-12 20:14:23 +0530

    Add files via upload

---
+++ Spring Assignments/Q3/Sword.java
@@ -0,0 +1,10 @@
+package mypack;
+
+public class Sword implements Weapon {
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("Attack with Sword");
+		
+	}
+}
